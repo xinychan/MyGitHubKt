c00926dba7bd1af468c08c53b24a0244d22beeb0
xinychan <xinychan@163.com>
2020-04-12 22:51:51 +0800

    完善 BaseFragment

--- mvp/src/main/java/com/example/xinychan/mvp/impl/BaseFragment.kt
+++ mvp/src/main/java/com/example/xinychan/mvp/impl/BaseFragment.kt
@@ -1,8 +1,11 @@
 package com.example.xinychan.mvp.impl
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.example.xinychan.mvp.IMvpView
 import com.example.xinychan.mvp.IPresenter
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
@@ -16,7 +19,10 @@ abstract class BaseFragment<out P : BasePresenter<IMvpView<P>>> : IMvpView<P>, F
         presenter.view = this
     }
 
-    fun createPresenterKt(): P {
+    /**
+     * 通过 Kotlin 反射获取泛型参数
+     */
+    private fun createPresenterKt(): P {
         kotlin.coroutines.experimental.buildSequence {
             // 先获取当前 BaseFragment 类（包括子类）
             var thisClass: KClass<*> = this@BaseFragment::class
@@ -36,8 +42,71 @@ abstract class BaseFragment<out P : BasePresenter<IMvpView<P>>> : IMvpView<P>, F
             return it.type!!.jvmErasure.primaryConstructor!!.call() as P
         }
     }
+
+    /**
+     * 通过 Java 反射获取泛型参数
+     */
+    private fun createPresenter(): P {
+        kotlin.coroutines.experimental.buildSequence<Type> {
+            var thisClass: Class<*> = this@BaseFragment.javaClass
+            while (true) {
+                yield(thisClass.genericSuperclass)
+                thisClass = thisClass.superclass ?: break
+            }
+        }.filter {
+            it is ParameterizedType
+        }.flatMap {
+            (it as ParameterizedType).actualTypeArguments.asSequence()
+        }.first {
+            it is Class<*> && IPresenter::class.java.isAssignableFrom(it)
+        }.let {
+            return (it as Class<P>).newInstance()
+        }
+    }
+
+    // 重写 Fragment 生命周期方法，调用 presenter 中的生命周期
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        presenter.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        presenter.onViewStateRestored(savedInstanceState)
+    }
 }
 
 // 测试类
 class MainFragment : BaseFragment<MainPresenter>()
+
 class MainPresenter : BasePresenter<MainFragment>()
