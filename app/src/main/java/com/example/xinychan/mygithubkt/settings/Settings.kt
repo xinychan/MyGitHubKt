3e5e1e45aeb0223eb2373303b0ba0ab519e23df3
xinychan <xinychan@163.com>
2020-04-14 23:19:51 +0800

    新增 LoginActivity 登录界面

---
+++ app/src/main/java/com/example/xinychan/mygithubkt/settings/Settings.kt
@@ -0,0 +1,5 @@
+package com.example.xinychan.mygithubkt.settings
+
+object Settings {
+
+}
---
+++ app/src/main/java/com/example/xinychan/mygithubkt/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.xinychan.mygithubkt.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.xinychan.mygithubkt.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
