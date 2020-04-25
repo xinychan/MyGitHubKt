e0c24fe6065ea678f82bf5a1135ff8c73917ea09
xinychan <xinychan@163.com>
2020-04-25 23:20:17 +0800

    替换 Configs 配置，实现登录自己的Github账号

--- app/src/main/java/com/example/xinychan/mygithubkt/settings/Configs.kt
+++ app/src/main/java/com/example/xinychan/mygithubkt/settings/Configs.kt
@@ -11,8 +11,8 @@ object Configs {
 
     object Account {
         val SCOPES = listOf("user", "repo", "notifications", "gist", "admin:org")
-        const val clientId = "cccb7d70ba4fe6d4f62d"
-        const val clientSecret = "30bea5fc021ed503bef21e23ce8e2b02d588ab6c"
+        const val clientId = "1cb79b0638991dd6669a"
+        const val clientSecret = "372fc0ad993c1c7a401614ab8387173b2c6e8630"
         const val note = "kotliner.cn"
         const val noteUrl = "http://www.kotliner.cn"
 
