e87d3e638a9c06f497ea00cdf89e8b1b66bf0935
xinychan <xinychan@163.com>
2020-05-04 23:53:02 +0800

    新增 AboutFragment

---
+++ app/src/main/java/com/example/xinychan/mygithubkt/view/fragments/AboutFragment.kt
@@ -0,0 +1,20 @@
+package com.example.xinychan.mygithubkt.view.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * 关于界面
+ */
+class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
