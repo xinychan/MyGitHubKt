a32f3e787fc15e0051fb935b6fa343e47afaeca2
xinychan <xinychan@163.com>
2020-05-05 13:17:45 +0800

    完善 MainActivity

--- app/src/main/java/com/example/xinychan/mygithubkt/MainActivity.kt
+++ app/src/main/java/com/example/xinychan/mygithubkt/MainActivity.kt
@@ -1,12 +1,108 @@
 package com.example.xinychan.mygithubkt
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.example.xinychan.common.ext.no
+import com.example.xinychan.common.ext.otherwise
+import com.example.xinychan.mygithubkt.model.account.AccountManager
+import com.example.xinychan.mygithubkt.model.account.OnAccountStateChangeListener
+import com.example.xinychan.mygithubkt.network.entities.User
+import com.example.xinychan.mygithubkt.utils.doOnLayoutAvailable
+import com.example.xinychan.mygithubkt.utils.loadWithGlide
+import com.example.xinychan.mygithubkt.view.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnAccountStateChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 实现 ActionBar 与 抽屉联动
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_layout.setDrawerListener(toggle)
+        toggle.syncState()
+
+        initNavigationView()
+
+        AccountManager.onAccountStateChangeListeners.add(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AccountManager.onAccountStateChangeListeners.remove(this)
+    }
+
+    private fun initNavigationView() {
+        AccountManager.currentUser?.let {
+            updateNavigationView(it)
+        } ?: clearNavigationView()
+        initNavigationHeaderEvent()
+    }
+
+    private fun initNavigationHeaderEvent() {
+        navigationView.doOnLayoutAvailable {
+            navigationHeader.setOnClickListener {
+                AccountManager.isLoggedIn().no {
+                    startLoginActivity()
+                }.otherwise {
+                    AccountManager.logout().subscribe(
+                        {
+                            Toast.makeText(this, "注销成功", Toast.LENGTH_SHORT).show()
+                        }, {
+                            it.printStackTrace()
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    private fun startLoginActivity() {
+        val intent: Intent = Intent()
+        intent.setClass(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun updateNavigationView(user: User) {
+        navigationView.doOnLayoutAvailable {
+            usernameView.text = user.login
+            emailView.text = user.email ?: ""
+            avatarView.loadWithGlide(user.avatar_url, user.login.first())
+        }
+    }
+
+    private fun clearNavigationView() {
+        navigationView.doOnLayoutAvailable {
+            usernameView.text = "请登录"
+            emailView.text = ""
+            avatarView.setImageResource(R.drawable.ic_github)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onLogin(user: User) {
+        updateNavigationView(user)
+    }
+
+    override fun onLogout() {
+        clearNavigationView()
     }
 }
