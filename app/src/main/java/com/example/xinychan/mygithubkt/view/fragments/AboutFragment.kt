764d5f7905677ac1aaab815cd1cb1a581c554e15
xinychan <xinychan@163.com>
2020-05-05 20:17:00 +0800

    点击展示 LicenseDialog

--- app/src/main/java/com/example/xinychan/mygithubkt/view/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/xinychan/mygithubkt/view/fragments/AboutFragment.kt
@@ -1,21 +1,43 @@
 package com.example.xinychan.mygithubkt.view.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.xinychan.mygithubkt.R
+import com.example.xinychan.mygithubkt.utils.markdownText
 
 /**
  * 关于界面
  */
 class AboutFragment : Fragment() {
+
+    // 初始化 licenses.md 是耗时操作，创建全局变量避免每次都初始化 licenses.md
+    private var mLicenseText: String? = null
+    private var mLicenseDialog: AlertDialog? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, null)
+        val view: View = View.inflate(context, R.layout.fragment_about, null)
+        val tvLicense: TextView = view.findViewById<TextView>(R.id.tv_license)
+        mLicenseText = context?.assets?.open("licenses.md")?.bufferedReader()?.readText().toString()
+        mLicenseDialog = licensesDialog()
+        tvLicense.setOnClickListener { mLicenseDialog?.show() }
+        return view
+    }
+
+    private fun licensesDialog(): AlertDialog {
+        val dialog: AlertDialog.Builder = AlertDialog.Builder(context)
+        val view: View = View.inflate(context, R.layout.dialog_license, null)
+        val tvContent: TextView = view.findViewById<TextView>(R.id.tv_license_content)
+        tvContent.markdownText = mLicenseText ?: "load license error"
+        dialog.setView(view)
+        return dialog.create()
     }
 }
